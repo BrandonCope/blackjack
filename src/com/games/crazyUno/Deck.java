37f2df792a103d89ff5827da50520becf14c378b
Brandon Copeland <bcopeland97@gmail.com>
2022-08-15 10:43:08 -0700

    created Deck and PlayPile classes

--- src/com/games/crazyUno/Deck.java
+++ src/com/games/crazyUno/Deck.java
@@ -1,7 +1,52 @@
 package com.games.crazyUno;
 
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 class Deck {
-    private Set<Card> cards;
+    // Fields
+    private static Map<Integer, Card> deckMap;
+
+
+    // Constructor
+    public Deck() {
+    }
+
+    public Deck(Map<Integer, Card> deckMap) {
+        this.deckMap = deckMap;
+    }
+
+
+
+
+    //Methods
+    /*
+     method creates deck by using all possible
+     variations of the card enum
+
+     */
+    public Map<Integer, Card> buildDeck(){
+
+        return deckMap;
+    }
+    /*
+     * gets top card from deckMap, and returns card
+     */
+    public static Map<Integer, Card> drawCard() {
+        Map<Integer, Card> result = deckMap;
+
+        return result;
+    }
+
+
+    public Map<Integer, Card> getDeckMap() {
+        return deckMap;
+    }
+
+    // resets deck
+    public void reset() {
+        deckMap = new TreeMap<>();
+        buildDeck();
+    }
 }
